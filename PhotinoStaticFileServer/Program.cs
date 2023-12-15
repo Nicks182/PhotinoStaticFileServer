222ba8fce0e638f03485446308b6a344258a46ea
Nicks182 <nic.s.bmw@gmail.com>
2023-12-15 14:01:55 +0200

    Working

---
+++ PhotinoStaticFileServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using PhotinoNET;
+using PhotinoNET.Server;
+using System.Reflection;
+
+namespace PhotinoStaticFileServer
+{
+    /*
+     * Note the changes in the project file...
+        <ItemGroup>
+		    <EmbeddedResource Include="Assets\**\*" />
+	    </ItemGroup>
+     */
+
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+
+            PhotinoServer
+            .CreateStaticFileServer(args: args, startPort: 8000, portRange: 100, webRootFolder: "Assets", out string baseUrl)
+            .RunAsync();
+
+            PhotinoWindow Wind = new PhotinoWindow();
+
+            Wind.Load($"{baseUrl}/index.html");
+
+            #region Addition functionality
+            Wind.RegisterWindowCreatedHandler(Win_WindowCreated);
+
+            Wind.RegisterWebMessageReceivedHandler((object sender, string message) =>
+            {
+                var window = (PhotinoWindow)sender;
+
+                // Send a message back the to JavaScript event handler.
+                // "window.external.receiveMessage(callback: Function)"
+                window.SendWebMessage("C# reveived the following message from the browser: " + message);
+            });
+            #endregion Addition functionality
+
+            Wind.WaitForClose();
+        }
+
+        private static void Win_WindowCreated(object? sender, EventArgs e)
+        {
+            // Using this event seems to be the only way to get Maximized to work
+            // Trying to set it before the window is created seems to not work...
+            (sender as PhotinoWindow).SetMaximized(true);
+        }
+
+
+    }
+}
---
+++ PhotinoStaticFileServer/Resources/Assets/script.js
@@ -0,0 +1,15 @@
+﻿
+alert("Hellow From script!");
+
+
+function callDotNet()
+{
+    window.external.sendMessage("Hi from browser.");
+}
+
+window.external.receiveMessage(message => receiveHandler(message));
+
+function receiveHandler(message)
+{
+    alert(message);
+}
